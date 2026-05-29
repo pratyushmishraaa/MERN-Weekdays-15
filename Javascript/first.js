50e69259ba7f64e679ed78a02e9829d46b4c7595
pratyushmishraaa <pratyushmishra287@gmail.com>
2026-05-29 16:15:31 +0530

    js intro

---
+++ Javascript/first.js
@@ -0,0 +1,12 @@
+setTimeout(function timeout() {
+    console.log("Click the button 1!");
+}, 5000);
+setTimeout(function timeout() {
+    console.log("Click the button 2!");
+}, 5000);
+setTimeout(function timeout() {
+    console.log("Click the button 3!");
+}, 5000);
+setTimeout(function timeout() {
+    console.log("Click the button 4!");
+}, 5000);
