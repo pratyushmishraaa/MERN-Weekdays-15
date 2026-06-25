fc74c66fc417e1eb873b3e0a783a397b3bda3eee
pratyushmishraaa <pratyushmishra287@gmail.com>
2026-06-25 23:34:14 +0530

    latest

--- project/backend/src/app.js
+++ project/backend/src/app.js
@@ -1,13 +1,23 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
+import { fileURLToPath } from "url";
 import authRouter from "../routes/auth.routes.js";
 import usersRouter from "../routes/users.routes.js";
 import ordersRouter from "../routes/order.routes.js";
 import productRouter from "../routes/product.routes.js";
 import errorMiddleware from "../middlewares/error.middle.js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const backendPublicPath = path.resolve(__dirname, "../public");
+const frontendDistPath = path.resolve(backendPublicPath, "dist");
+
 const app = express();
+
+app.use(express.static(backendPublicPath));
+app.use(express.static(frontendDistPath));
 
 app.use(cors({
   origin: "http://localhost:5173",
@@ -24,6 +34,10 @@ app.use("/api/products/v1", productRouter);
 
 app.get("/api/health", (req, res) => {
   res.send("OK");
+});
+
+app.get(/^\/(?!api).*/, (req, res) => {
+  res.sendFile(path.join(frontendDistPath, "index.html"));
 });
 
 app.use(errorMiddleware);
